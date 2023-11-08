d7009b902dd9d84f8d0ee7212006afd9d83366bb
NAMA KAMALAKARA NAGA SANTHOSH KUMAR <32108020+ailu96@users.noreply.github.com>
2023-11-08 16:00:17 -0600

    Update urls.py

--- backendHotel/urls.py
+++ backendHotel/urls.py
@@ -6,7 +6,6 @@ urlpatterns=[
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('management/',login_required(views.homepage),name='homepage'),
-    path('menu/', login_required(views.menu), name='menu'),
     path('get_cuisine_items/', login_required(views.get_cuisine_items), name='get_cuisine_items'),
     path('order/', login_required(views.order), name='order'),
     path('get_all_items/', login_required(views.get_all_items), name='get_all_items'),
