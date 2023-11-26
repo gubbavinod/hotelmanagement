c9e3a09ac06eb8b9ff72671eaff1b435fddaca70
NAMA KAMALAKARA NAGA SANTHOSH KUMAR <32108020+ailu96@users.noreply.github.com>
2023-11-26 00:50:38 -0500

    Add files via upload

--- backendHotel/testingcopy.py
+++ backendHotel/testingcopy.py
@@ -80,6 +80,58 @@ def get_items_list():
 
 
 
+
+
+
+''' 
+# SQL query to insert values into the 'customers' table
+insert_customer_query = """
+INSERT INTO customers (username, customer_name, password, phone_number, mail_id)
+VALUES (%s, %s, %s, %s, %s);
+"""
+
+# Values to insert into the 'customers' table
+customer_values = [
+    ('santhosh96', 'santhosh nama', 'test123', 8123456789, 'santhoshtest@gmail.com'),
+    ('john_doe', 'John Doe', 'password123', 1234567890, 'john.doe@example.com'),
+    ('alice_smith', 'Alice Smith', 'alice_password', 9876543210, 'alice.smith@example.com')
+]
+
+# Execute the query for each set of values
+for values in customer_values:
+    execute_query(insert_customer_query, values)
+
+    '''
+
+
+def execute_query1(query):
+    """Connects to the MySQL database, executes the provided query, and returns the result."""
+    try:
+        connection = mysql.connector.connect(**db_params)
+        if connection:
+            cursor = connection.cursor()
+
+            # Execute the provided query
+            cursor.execute(query)
+
+            # Fetch the result
+            result = cursor.fetchall()
+
+            cursor.close()
+
+            return result
+
+    except mysql.connector.Error as e:
+        print(f"Error executing query: {e}")
+        return None
+
+    finally:
+        if connection.is_connected():
+            connection.close()
+            print("Connection closed.")
+
+
+
 def get_pastOrdersDB(orderedby=None):
     """Retrieves the list of items from the 'meal_info_updated' table."""
     try:
@@ -116,6 +168,23 @@ def get_pastOrdersDB(orderedby=None):
         close_connection(connection)
 
 
+query_result=get_pastOrdersDB()
+
+# Example usage:
+#query_result = execute_query1("SELECT id,customername,items,price from orders where orderedby='santhosh96'")
+#query_result=execute_query1('SELECT id, customername, items, price FROM orders;')
+
+
+
+#query_result = execute_query1("""INSERT INTO managers (username, password, name, mailid) VALUES ('manager1', 'test123', 'Kamal Hassan', 'kamal96@gmail.com');""")
+
+if query_result:
+    print("Query Result:")
+    for row in query_result:
+        print(row)
+else:
+    print("No result or error in executing the query.")
+
 def execute_query(query, values=None):
     """Connects to the MySQL database, executes the provided query with optional values, and returns the result."""
     try:
@@ -145,34 +214,24 @@ def execute_query(query, values=None):
             connection.close()
             print("Connection closed.")
 
+def insert_order(customer_name ,items, orderedBy, totalPrice):
+    """Inserts a new order into the 'orders' table."""
+   
+    query = """
+    INSERT INTO orders (customername ,items, orderedby, price)
+    VALUES (%s, %s, %s, %s);
+    """
+    values = (customer_name ,items, orderedBy, totalPrice)
 
-def customer_authentication(username, password):
-    """Authenticates a customer based on username and password."""
-    query = "SELECT * FROM customers WHERE username = %s AND password = %s;"
-    values = (username, password)
-
-    result = execute_query(query, values)
-
-    if result:
+    try:
+        execute_query(query, values)
         return True
-    else:
-       
-        return False
-
-
-def manager_authentication(username, password):
-    """Authenticates a customer based on username and password."""
-    query = "SELECT * FROM managers WHERE username = %s AND password = %s;"
-    values = (username, password)
-
-    result = execute_query(query, values)
-
-    if result:
-        return True
-    else:
-       
+    except:
         return False
     
+
+#insert_order('pawan kalyan' ,'Butter Chicken,Biryani', 'manager', 54)
+
 def insert_customer(customer_name, username, password, mail_id, phone_number):
     """Inserts a new customer into the 'customers' table."""
    
@@ -187,19 +246,78 @@ def insert_customer(customer_name, username, password, mail_id, phone_number):
         return True
     except:
         return False
-    
 
-def insert_order(customer_name ,items, orderedBy, totalPrice):
-    """Inserts a new order into the 'orders' table."""
+
+def insert_manager(name, username, password, mail_id):
+    """Inserts a new customer into the 'customers' table."""
    
     query = """
-    INSERT INTO orders (customername ,items, orderedby, price)
+    INSERT INTO managers (name, username, password, mailid)
     VALUES (%s, %s, %s, %s);
     """
-    values = (customer_name ,items, orderedBy, totalPrice)
+    values = (name, username, password, mail_id)
 
     try:
         execute_query(query, values)
-        return values
+        return True
     except:
         return False
+    
+#insert_manager(username='manager1', password='test123', name='Kamal Hassan', mail_id='kamal96@gmail.com')    
+    
+"""
+
+
+
+def execute_query12(query, values=None):
+    #Connects to the MySQL database, executes the provided query with optional values, and returns the result
+    try:
+        connection = mysql.connector.connect(**db_params)
+        if connection:
+            cursor = connection.cursor()
+
+            # Execute the provided query with optional values
+            cursor.execute(query, values)
+
+            # Fetch the result (if applicable)
+            result = None
+            if cursor.with_rows:
+                result = cursor.fetchall()
+
+            # Commit changes to the database
+            connection.commit()
+
+            cursor.close()
+            return result  # Return the result to the caller
+
+    except mysql.connector.Error as e:
+        print(f"Error executing query: {e}")
+
+    finally:
+        if connection.is_connected():
+            connection.close()
+            print("Connection closed.")
+
+
+def customer_authentication(username, password):
+ #Authenticates a customer based on username and password
+    query = "SELECT * FROM customers WHERE username = %s AND password = %s;"
+    values = (username, password)
+
+    result = execute_query12(query, values)
+
+    if result:
+        return True
+    else:
+       
+        return False
+
+# Example usage:
+username_input = "santhosh96"
+password_input = "test123"
+
+customer_authentication(username_input, password_input)
+
+"""
+
+#print(insert_customer(customer_name='mohan Muappalla',username='mohan98',password= 'test123',mail_id= 'mohan98@gmail.com',phone_number= '8323124451'))
--- backendHotel/views.py
+++ backendHotel/views.py
@@ -5,33 +5,93 @@ from django.contrib.auth import logout
 from django.contrib import messages
 from django.shortcuts import render
 from django.http import JsonResponse
-import csv
+
+import json
 
 from backendHotel.databaseconnection import *
 
-def login_view(request):
+
+def set_user_type(request, user_type):
+    """
+    Helper function to set the user type in the session.
+    """
+
+    request.session['user_type'] = user_type
+
+def set_loginStatus(request, user_status):
+    """
+    Helper function to set the user login status in the session.
+    """
+    
+    request.session['user_status'] = user_status
+
+def set_UserName(request, userName):
+    """
+    Helper function to set the user Name in the session.
+    """
+    request.session['userName'] = userName
+
+
+def signup(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
+        customer_name = request.POST.get('customer_name')
         password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('homepage')  # Redirect to the homepage upon successful login
+        username = request.POST.get('username')
+        mailid = request.POST.get('mailid')
+        phone_number = request.POST.get('phone_number')
+        result=insert_customer(customer_name ,username, password, mailid, phone_number)
+        if result:
+            return redirect('login')  # Redirect to the homepage upon successful login
         else:
-            return render(request, 'login.html', {'error': 'Invalid credentials'})
+            return render(request, 'login', {'error': 'Invalid request'})
     
     return render(request, 'login.html')
 
-@login_required
-def homepage(request):
-    if not request.user.is_authenticated: #if the user is not authenticated
-        return redirect("login")
-    else:
-        return render(request, 'homepage.html')
+def customer_login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')  # Replace 'username' with the actual form field name
+        password = request.POST.get('password')  # Replace 'password' with the actual form field name
 
-@login_required
+        if customer_authentication(username, password):
+            set_user_type(request, 'customer')
+            set_loginStatus(request, True)
+            set_UserName(request, username)
+            return redirect('homepage')  # Redirect to the homepage upon successful login
+        else:
+
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    
+    return render(request, 'login')
+
+def manager_login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if manager_authentication(username, password):
+            set_user_type(request, 'manager')
+            set_loginStatus(request, True)
+            return redirect('homepage')
+        else:
+            error_message = 'Invalid credentials'
+            return render(request, 'login.html', {'error': error_message})
+
+    return render(request, 'login.html')
+
+
+
+def homepage(request):
+    if request.session.get('user_status',''):
+        return render(request, 'homepage.html')  #if the user is not authenticated
+    else:
+        return redirect("login")
+
+
+
 def logout_view(request):
     logout(request)
+    set_user_type(request, '')
+    set_loginStatus(request,False)
     messages.info(request,"Logged Out Successfully")
     return redirect('/Hotel/login')  # Redirect to the login page after logging out
 
@@ -45,16 +105,16 @@ def get_cuisine_items(request):
     return JsonResponse(items, safe=False)
 
 
-@login_required
+
 def menu(request):
-    if not request.user.is_authenticated: #if the user is not authenticated
+    if not request.session.get('user_status',''): #if the user is not authenticated
         return redirect("menu")
     else:
         return render(request, 'menu.html')
         
-@login_required
+
 def order(request):
-    if not request.user.is_authenticated: #if the user is not authenticated
+    if not request.session.get('user_status',''): #if the user is not authenticated
         return redirect("login")
     else:
         return render(request, 'order.html')
@@ -67,3 +127,56 @@ def get_all_items(request):
     return JsonResponse(items, safe=False)
 
 
+def get_pastOrders(request):
+    
+    # Call the function to get the items
+    if(request.session.get('user_type', '') =='customer'):
+        username=request.session.get('userName', '')
+        items = get_pastOrdersDB(username)
+    else:
+        items = get_pastOrdersDB()
+    return JsonResponse(items, safe=False)
+
+
+
+def contactUS(request):
+    if not request.session.get('user_status',''):  #if the user is not authenticated
+        return redirect("login")
+    else:
+        return render(request, 'contactUS.html')
+
+
+def about(request):
+    if not request.session.get('user_status',''):  #if the user is not authenticated
+        return redirect("login")
+    else:
+        return render(request, 'about.html')
+
+
+def reservation(request):
+    if not request.session.get('user_status',''): #if the user is not authenticated
+        return redirect("login")
+    else:
+        return render(request, 'reservation.html')
+    
+
+def placeOrder(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        customer_name = data.get('customerName')
+        items = str(data.get('items'))
+        totalPrice = data.get('totalPrice')
+        # Assuming your `insert_order` function returns a boolean indicating success
+        if request.session.get('user_type', '') == 'customer':
+            result = insert_order(customer_name, items, request.session.get('userName', ''), totalPrice)
+        else:
+            result = insert_order(customer_name, items, 'manager', totalPrice)
+        
+        if result:
+            # Return a JSON response indicating success
+            return JsonResponse({'status': 'success'})
+        else:
+            # Return a JSON response indicating failure
+            return JsonResponse({'status': 'failure'})
+    
+    return render(request, 'order.html')
